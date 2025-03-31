b837c1fca556a031128b64eb5c7e95874ed6f565
Esteban <estebanpiazza51@gmail.com>
2025-03-31 20:52:26 -0300

    clase 3

---
+++ prompt.js
@@ -0,0 +1,7 @@
+var prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+var n = prompt('How many more times? ');
+let m = prompt("Pone tu nombre, perro")
+console.log(`hola ${m}, ingresaste el numero ${n+1}`)
